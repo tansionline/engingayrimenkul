81fa6c64c37436c7f7d678da78aead3e7aaf99cf
selcuk <tatarselcuk27@gmail.com>
2020-05-09 10:32:15 +0300

    whatsapp button change and new something

--- main.js
+++ main.js
@@ -65,3 +65,47 @@ window.jssor_1_slider_init = function() {
   $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
   /*#endregion responsive code end*/
 };
+
+
+// ILAN  SLIDERLARI
+
+function slideshow() {
+  // clone
+  $('.slider-1').clone().removeClass('slider-1').addClass('slider-2').insertAfter($('.slider'));
+
+  // set first
+  $('.slider-1').slick({
+    draggable: false,
+    dots: false,
+    infinite: true,
+    responsive: true,
+    asNavFor: '.slider-2',
+    touchThreshold: 20,
+    speed: 1000,
+    fade: true
+  });
+
+  // set second
+  $('.slider-2').slick({
+    dots: true,
+    infinite: true,
+    responsive: true,
+    asNavFor: '.slider-1',
+    arrows: false,
+    speed: 1000,
+    easing: 'easeInOutQuart'
+  });
+}
+
+$(function() {
+  slideshow();
+  setTimeout(function() {
+    $('.slider-1 .slick-next').click();
+  }, 1000);
+})
+
+
+
+
+
+// ANASAYFA ILANLARIN SLIDER JS KODLARI DENEME1 
